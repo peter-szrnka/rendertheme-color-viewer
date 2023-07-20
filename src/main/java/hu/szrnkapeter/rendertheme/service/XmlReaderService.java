6b39e896755c0f40aa83e332efc037ea64895a9e
Péter Szrnka <szrnka.peter@gmail.com>
2023-07-20 13:34:32 +0200

    Enable secure processing

--- src/main/java/hu/szrnkapeter/rendertheme/service/XmlReaderService.java
+++ src/main/java/hu/szrnkapeter/rendertheme/service/XmlReaderService.java
@@ -3,6 +3,7 @@ package hu.szrnkapeter.rendertheme.service;
 import java.io.File;
 import java.io.IOException;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -15,6 +16,10 @@ public interface XmlReaderService {
 
 	public static NodeList getNodeList(File fXmlFile) throws SAXException, IOException, ParserConfigurationException {
 		final DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		dbFactory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+		dbFactory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+		dbFactory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+
 		final DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 		final Document doc = dBuilder.parse(fXmlFile);
 		doc.getDocumentElement().normalize();
